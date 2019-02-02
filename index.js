8fa1ce176abe0f224f1e7b6f913e0c32a81b7448
Amanda Murphy <helloamandamurphy@gmail.com>
2019-02-02 18:52:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@
 
 function scuberGreetingForFeet (feet) {
   if (feet <= 400) {
-    return 'This one is on me!';
+    return "This one is on me!";
   } else if (1999 < feet && feet < 2500) {
-    return 'I will gladly take your thirty bucks.';
+    return "I will gladly take your thirty bucks.";
     } else {
     return "No can do.";
   }
